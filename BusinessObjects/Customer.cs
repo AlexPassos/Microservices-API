c530fbbf58f3409d90125b1767c773c4116f8174
Alex Passos <passos_alex@hotmail.com>
2024-02-10 10:57:33 -0300

    Testando commit

--- BusinessObjects/Customer.cs
+++ BusinessObjects/Customer.cs
@@ -1,7 +1,10 @@
-﻿namespace BusinessObjects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessObjects
 {
     public class Customer
     {
-
+        [Key]
+        public long Id { get; set; }
     }
 }
